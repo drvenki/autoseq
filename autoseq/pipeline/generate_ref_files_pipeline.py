776da3088d8ac2ce7fcc45e5594ce1b58aad1297
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-04-20 09:14:29 +0200

    update path to remote dbsnp vcf

--- autoseq/pipeline/generate_ref_files_pipeline.py
+++ autoseq/pipeline/generate_ref_files_pipeline.py
@@ -32,7 +32,7 @@ class GenerateRefFilesPipeline(PypedreamPipeline):
         self.reference_data = dict()
 
         self.exac_remote = "ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz"
-        self.dbsnp_remote = "ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b142_GRCh37p13/VCF/00-All.vcf.gz"
+	self.dbsnp_remote = "ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b147_GRCh37p13/VCF/All_20160408.vcf.gz"
         self.clinvar_remote = "ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/archive/2016/clinvar_20160203.vcf.gz"
         self.icgc_somatic_remote = "https://dcc.icgc.org/api/v1/download?fn=/release_20/Summary/simple_somatic_mutation.aggregated.vcf.gz"
         self.ypar_remote = "https://raw.githubusercontent.com/dakl/genome/master/ypars.bed"
