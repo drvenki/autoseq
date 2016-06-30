e8cbca42c0f49c46073796865ff2a99cead5244a
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-06-30 12:41:44 +0200

    create condo-list-tests.txt with stuff to install for any tests to run

--- tests/setup-tests.sh
+++ tests/setup-tests.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Get and install anaconda for custom Python installation
+echo -n "Downloading conda... "
+wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh 2> /dev/null > /dev/null
+echo "Done!"
+bash Miniconda-latest-Linux-x86_64.sh -b -p $HOME/miniconda2 -f
+conda config --add channels r
+conda config --add channels bioconda
+
+conda config --set always_yes yes --set changeps1 no
+# - conda update -q conda
+# Useful for debugging any issues with conda
+conda info -a
+conda install --file conda-list-tests.txt
+
+
 TEST_FILE=$1
 if [[ ${TEST_FILE} == integration* ]];
 then
@@ -18,20 +33,10 @@ then
     wget --no-clobber -O /tmp/test-libraries.tar.gz https://export.uppmax.uu.se/b2010040/test-libraries.tar.gz 2> /dev/null > /dev/null
     tar xvfz /tmp/test-libraries.tar.gz -C /tmp 2> /dev/null > /dev/null
 
-      # latex
+    # latex
     time sudo apt-get -y install texlive-latex-base 2> /dev/null > /dev/null
     time sudo apt-get -y install latex-xcolor texlive-fonts-recommended texlive-latex-extra 2> /dev/null > /dev/null
 
-    # Get and install anaconda for custom Python installation
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh 2> /dev/null > /dev/null
-    bash Miniconda-latest-Linux-x86_64.sh -b -p $HOME/miniconda2 -f 2> /dev/null > /dev/null
-    conda config --add channels r 2> /dev/null > /dev/null
-    conda config --add channels bioconda 2> /dev/null > /dev/null
-
-    conda config --set always_yes yes --set changeps1 no 2> /dev/null > /dev/null
-    # - conda update -q conda
-    # Useful for debugging any issues with conda
-    # conda info -a
+    # binaries for pipeline/s
     conda install --file conda-list.txt
-
 fi
