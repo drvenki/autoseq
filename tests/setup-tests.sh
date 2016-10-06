9a1f94de400150341d7e2d638e4a6fa1c2a26fdd
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-10-06 15:04:44 +0200

    no need to travis_wait

--- tests/setup-tests.sh
+++ tests/setup-tests.sh
@@ -5,13 +5,14 @@ echo -n "Downloading conda... "
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh 2> /dev/null > /dev/null
 echo "Done!"
 bash Miniconda-latest-Linux-x86_64.sh -b -p $HOME/miniconda2 -f
+
 conda config --add channels r
 conda config --add channels bioconda
 conda config --add channels dakl
 
 conda config --set always_yes yes --set changeps1 no
-# - conda update -q conda
+
 # Useful for debugging any issues with conda
 conda info -a
-conda install --file conda-list-tests.txt
 
+conda install --file conda-list-tests.txt --quiet
