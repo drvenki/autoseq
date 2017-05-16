e857993bf021f8105f533700ffa9843f1efabb0f
Thomas Whitington <thomaswhitington@gmail.com>
2017-05-16 16:40:43 +0200

    Breaking order form up further.

--- autoseq/util/orderform.py
+++ autoseq/util/orderform.py
@@ -4,44 +4,46 @@ from openpyxl import load_workbook
 from clinseq_barcode import *
 
 
+def parse_orderform_block(block_of_values)
+    # Look in the entries between <SAMPLE ENTRIES> and </SAMPLE ENTRIES> for clinseq barcodes: 
+    validated_clinseq_barcodes = []
+    clinseq_barcodes_section = False
+    for cell_value in block_of_values:
+        if cell_value == "</SAMPLE ENTRIES>":
+            clinseq_barcodes_section = False
+        if clinseq_barcodes_section:
+            if not clinseq_barcode_is_valid(cell_value):
+                raise ValueError("Invalid clinseq barcode: " + cell_value)
+            validated_clinseq_barcodes.append(cell_value)
+        if cell_value == "<SAMPLE ENTRIES>":
+            clinseq_barcodes_section = True
+        else:
+            logging.debug("Ignoring field from order form: " + cell_value)
+
+    return validated_clinseq_barcodes
+
+
+def parse_orderform_worksheet(order_form_worksheet):
+    """
+    Extract clinseq barcodes from the given order form excel spreadsheet worksheet.
+
+    :param order_form_worksheet: An openpyxl worksheet.
+    :return: List of clinseq barcodes extracted from the worksheet.
+    """
+
+    first_column_vals = [row[0].value for row in list(order_form_worksheet.iter_rows()) if
+                         row[0].value is not None]
+
+    return parse_orderform_block(first_column_vals)
+
+
 def parse_orderform(order_form_filename):
     """
-    Parse clinseq barcodes from the specified order form.
+    Extract clinseq barcodes from the specified order form.
 
     :param order_form_filename: An excel spreadsheet filename.
     :return: List of clinseq barcodes extracted from the order form.
     """
 
     workbook = load_workbook(order_form_filename)
-    worksheet = workbook.worksheets[0]
-    first_idx = None
-
-    libraries = []
-
-    for idx in range(1, 10000):
-        cell_value = worksheet.cell(column=1, row=idx).value
-        if cell_value == "<SAMPLE ENTRIES>":
-            first_idx = idx
-
-        # no need to parse after end tag
-        if cell_value == "</SAMPLE ENTRIES>":
-            break
-
-        if not cell_value:
-            continue
-
-        if first_idx is not None and idx > first_idx:
-            clinseq_barcode = str(worksheet.cell(column=1, row=idx).value)
-
-            logging.debug("Parsed library {}".format(clinseq_barcode))
-            sample_type = parse_sample_type(clinseq_barcode)
-            if sample_type not in ['T', 'N', 'CFDNA']:
-                logging.warning(
-                    "Unexpected library type detected: {} for library {}".format(sample_type, clinseq_barcode))
-
-            if not clinseq_barcode_is_valid(clinseq_barcode):
-                raise ValueError("Invalid clinseq barcode: " + clinseq_barcode)
-
-            libraries.append(clinseq_barcode)
-
-    return libraries
+    return parse_orderform_worksheet(workbook.worksheets[0])
